3ea19173981235f5a2fa59e95fc41a90e85cbac0
satyA-i <imsatya522@gmail.com>
2026-06-12 18:14:00 +0530

    Add program to check if two numbers are equal

---
+++ Checking_Two_Entered_Numbers_Are_Equal_Or_Not.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include <math.h>
+
+int main()
+
+{
+
+    int Num1 , Num2;
+
+    printf("Enter First Number --> ");
+
+    scanf("%d" , &Num1);
+
+    printf("Enter Second Number --> ");
+
+    scanf("%d" , &Num2);
+
+    if(Num1 == Num2)
+
+    {
+
+        printf("They are Equal");
+
+    }
+
+    else
+
+    {
+
+        printf("They are not Equal");
+
+    }
+
+    return 0;
+
+}
